43502a4a9f7b915c27974a0d46c47aaac4c2e62b
burtonzr <burtonzr@gmail.com>
2020-06-27 12:16:40 -0500

    Display model type image based off of id.

--- templates/ModelTypes/index.php
+++ templates/ModelTypes/index.php
@@ -4,7 +4,23 @@
         <?php foreach ($modelTypes as $modelType): ?>
             <div class="col-12 col-sm-6 content">
                 <h3 class="text-center" style="text-transform: capitalize;"><?= $this->Html->link(__(h($modelType->code)), ['action' => 'view', $modelType->id]) ?></h3>
-                <img src="http://www.modelersgallery.com/dev-alcapwin/img/gallery/defaults/spacecraft.jpg" />
+                <?php if($modelType->id == 1) { ?>
+                    <img src="img/naval.jpg" class="img-fluid" />
+                <?php } else if ($modelType->id == 2) { ?>
+                    <img src="img/aircraft.jpg" class="img-fluid" />
+                <?php } else if ($modelType->id == 3) { ?>
+                    <img src="img/automotive.jpg" class="img-fluid" />
+                <?php } else if ($modelType->id == 4) { ?>
+                    <img src="img/armor.jpg" class="img-fluid" />
+                <?php } else if ($modelType->id == 5) { ?>
+                    <img src="img/figures.jpg" class="img-fluid" />
+                <?php } else if ($modelType->id == 6) { ?>
+                    <img src="img/trains.jpg" class="img-fluid" />
+                <?php } else if ($modelType->id == 7) { ?>
+                    <img src="img/dioramas.jpg" class="img-fluid" />
+                <?php } else if ($modelType->id == 8) { ?>
+                    <img src="img/spacecraft.jpg" class="img-fluid" />
+                <?php } ?>
             </div>
         <?php endforeach; ?>
     </div>
